196eab25203313385dcf45fee28a8c07a23ed0bf
Trihan Marsudi <trihanmarsudi@gmail.com>
2021-06-04 14:50:55 +0700

    Convert HTML to svelte components

---
+++ src/data/charities.js
@@ -0,0 +1,16 @@
+module.exports = {
+    charities: [
+        {
+            id: 1,
+            title: 'First Charity Project',
+            category: 'Money',
+            thumbnail:'/assets/images/slide2.png',
+            pledged:99000,
+            target: 100000,
+            date_end: +new Date('10 August 2021'),
+            profile_photo: '/assets/images/avatar/avatar_1.jpg',
+            profile_name: 'Viroide BUeno',
+            no_pledges:0
+        }
+    ]
+}
